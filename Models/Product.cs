136d8ae0dbdc201d747bbe200a9b0bb6d1e5338b
Alex Coronell <alexander@catapultsteel.com>
2024-08-07 14:04:38 -0500

    Save product completed

--- Models/Product.cs
+++ Models/Product.cs
@@ -3,9 +3,9 @@ namespace blazor_app_demo;
 public class Product
 {
     public int Id { get; set; }
-    public required string Title { get; set; }
+    public string Title { get; set; }
     public decimal? Price { get; set; }
-    public required string Description { get; set; }
+    public string Description { get; set; }
     public int CategoryId { get; set; }
 
     public string[]? Images { get; set; }
